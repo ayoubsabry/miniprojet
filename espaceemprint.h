a32b5377c1be199495c3eee3565fc4c04f53a7c8
ayoubsabry <135043260+ayoubsabry@users.noreply.github.com>
2023-06-01 01:05:54 +0200

    Create espaceemprint.h

---
+++ espaceemprint.h
@@ -0,0 +1,15 @@
+#ifndef ESPACEEMPRINTE_H_INCLUDED
+#define ESPACEEMPRINTE_H_INCLUDED
+
+
+void incrnmbredelivreutil(char*);
+void etudientemprint0(char*);
+void emprinter(void);
+void returner(void);
+void listertableau(char*,char*,char*);
+void listerdemonds(void);
+void listerEmprinter(void);
+void suprimerdelalistedemonde(void);
+void passagverEmprinte(void);
+
+#endif // ESPACEEMPRINTE_H_INCLUDED
