02de6683e2e3c1e81ae19d726c9d7f920f4ef313
ayoubsabry <135043260+ayoubsabry@users.noreply.github.com>
2023-06-01 01:10:48 +0200

    Create etudient.h

---
+++ etudient.h
@@ -0,0 +1,12 @@
+#ifndef ETUDIENT_H_INCLUDED
+#define ETUDIENT_H_INCLUDED
+
+const char* ecretudientdansliste="%s,,%s,,%s\n"; //titrelivre  codapo   datedomm
+  const char* liretudientdansliste="%[^,,],,%[^,,],,%[^\n]\n";
+  void etudientmenu(char*);
+void verificationComptetudient(void);
+void emprinterlivre(char*);
+void livremprinter(char*);
+
+
+#endif // ETUDIENT_H_INCLUDED
